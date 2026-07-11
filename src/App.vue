fd20be5d31828cc67ff7c9b7857a9b1e1bf1ad43
Ibrahim H <bitsnaps@yahoo.fr>
2026-07-11 11:13:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import CButton from '@/CButton.vue'
+import { onMounted, ref  } from 'vue'
+
+const users = ref([]);
+
+onMounted(async () => {
+
+  console.log('fetch users..');
+
+  users.value = await fetch('users.json')
+    .then((r) => r.json())
+    .then((data) => data)
+  
+})
+
+const showAlert = () => {
+  console.log("ok")
+}
+
+</script>
+
+<template>
+  <div>
+
+    <h1>You did it!</h1>
+
+    <CButton variant="success" @callAction="showAlert">OK</CButton>
+
+    <p>There are {{ users?.length }} user(s).</p>
+    <ul>
+      <li v-for="user in users">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
+
+<style scoped></style>
---
+++ src/CButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button :class="`btn btn-${variant}`" @click="$emit('callAction')">
+        <slot />
+    </button>
+</template>
+
+<script setup>
+defineProps({
+    variant: String
+})
+
+</script>
+
+<style scoped>
+.btn-success {
+    background-color: green;
+}
+</style>
